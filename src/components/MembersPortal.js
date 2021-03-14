e88ea8a3f3f50fadc0c86548a319d361e5c33429
Albert Chang <a89529294@hotmail.com>
2021-03-14 17:49:15 +0800

    .

--- src/components/MembersPortal.js
+++ src/components/MembersPortal.js
@@ -1,20 +1,21 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid } from '@material-ui/core';
+import { Grid, Paper } from '@material-ui/core';
 import { Typography } from '@material-ui/core';
 import SwiperCore, { EffectCube } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-import { title } from '../static/membersContent';
-import img1 from '../assets/eventOneImage.jpeg';
-import img2 from '../assets/eventTwoImage.jpeg';
-
 import 'swiper/swiper.min.css';
 import 'swiper/components/effect-cube/effect-cube.min.css';
 
+import {
+  title,
+  maleMemberInfoArr,
+  femaleMemberInfoArr,
+} from '../static/membersContent';
+
 SwiperCore.use([EffectCube]);
 
-const imgArr = [img1, img2];
+const memberImageHeight = '20rem';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,7 +27,7 @@ const useStyles = makeStyles((theme) => ({
     textAlign: 'center',
   },
   carouselContainer: {
-    height: '20rem',
+    height: memberImageHeight,
   },
   swiperContainer: {
     height: '100%',
@@ -43,38 +44,73 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
     objectFit: 'cover',
   },
+  memberDesc: {
+    width: '100%',
+    height: memberImageHeight,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 }));
+
+const SwiperCube = ({ infoArr, setIndex }) => {
+  const classes = useStyles();
+  return (
+    <Swiper
+      effect="cube"
+      className={classes.swiperContainer}
+      loop={true}
+      cubeEffect={{
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+      }}
+      grabCursor={true}
+      onSlideChange={(e) => setIndex(e.realIndex)}
+    >
+      {infoArr.map((infoObj, i) => (
+        <SwiperSlide className={classes.swiperSlide} key={i}>
+          <img src={infoObj.img} className={classes.swiperImg} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
 
 export default function MembersPortal() {
   const classes = useStyles();
+  const [maleIndex, setMaleIndex] = useState(0);
+  const [femaleIndex, setFemaleIndex] = useState(0);
 
   return (
-    <Grid container className={classes.root}>
+    <Grid container className={classes.root} spacing={1} justify="center">
       <Grid item xs={12}>
         <Typography className={classes.title}>{title}</Typography>
       </Grid>
-      <Grid item lg={4} className={classes.carouselContainer}>
-        <Swiper
-          effect="cube"
-          className={classes.swiperContainer}
-          loop={true}
-          cubeEffect={{
-            shadow: true,
-            slideShadows: true,
-            shadowOffset: 20,
-            shadowScale: 0.94,
-          }}
-          grabCursor={true}
-          onSlideChange={(e) => console.log(e.realIndex)}
-        >
-          {imgArr.map((imgSrc, i) => (
-            <SwiperSlide className={classes.swiperSlide} key={i}>
-              <img src={imgSrc} className={classes.swiperImg} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+      <Grid item lg={2} className={classes.carouselContainer}>
+        <SwiperCube infoArr={maleMemberInfoArr} setIndex={setMaleIndex} />
       </Grid>
-      <Grid item lg={8}></Grid>
+      <Grid item lg={3} className={classes.memberDesc} direction="column">
+        <Typography variant="h5">
+          {maleMemberInfoArr[maleIndex].name}
+        </Typography>
+        <Typography color="textSecondary">
+          {maleMemberInfoArr[maleIndex].age}歲{' '}
+        </Typography>
+        <Typography color="textSecondary">
+          {maleMemberInfoArr[maleIndex].height}公分{' '}
+        </Typography>
+        <Typography color="textSecondary">
+          {maleMemberInfoArr[maleIndex].profession}
+        </Typography>
+      </Grid>
+      <Grid item lg={3}>
+        {femaleMemberInfoArr[femaleIndex].name}
+      </Grid>
+      <Grid item lg={2} className={classes.carouselContainer}>
+        <SwiperCube infoArr={femaleMemberInfoArr} setIndex={setFemaleIndex} />
+      </Grid>
     </Grid>
   );
 }
--- src/static/membersContent.js
+++ src/static/membersContent.js
@@ -1,3 +1,30 @@
-const title = '優質會員';
+import m1 from '../assets/m1.jpeg';
+import m2 from '../assets/m2.jpeg';
+import m3 from '../assets/m3.jpeg';
+import m4 from '../assets/m4.jpeg';
+import m5 from '../assets/m5.png';
 
-export { title };
+import f1 from '../assets/f1.jpeg';
+import f2 from '../assets/f2.jpeg';
+import f3 from '../assets/f3.jpeg';
+import f4 from '../assets/f4.jpeg';
+import f5 from '../assets/f5.jpeg';
+
+const title = '優質會員';
+const maleMemberInfoArr = [
+  { img: m1, name: '張先生', age: 35, height: 190, profession: 'accountant' },
+  { img: m2, name: '楊先生', age: 36, height: 180, profession: 'banker' },
+  { img: m3, name: '李先生', age: 37, height: 170, profession: 'professor' },
+  { img: m4, name: '蘇先生', age: 38, height: 160, profession: 'entrepreneur' },
+  { img: m5, name: '陳先生', age: 39, height: 150, profession: 'unemployed' },
+];
+
+const femaleMemberInfoArr = [
+  { img: f1, name: '張小姐', age: 35, height: 190, profession: 'accountant' },
+  { img: f2, name: '楊小姐', age: 36, height: 180, profession: 'banker' },
+  { img: f3, name: '李小姐', age: 37, height: 170, profession: 'professor' },
+  { img: f4, name: '蘇小姐', age: 38, height: 160, profession: 'entrepreneur' },
+  { img: f5, name: '陳小姐', age: 39, height: 150, profession: 'unemployed' },
+];
+
+export { title, maleMemberInfoArr, femaleMemberInfoArr };
